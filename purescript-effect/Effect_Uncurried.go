5eebadd4a8f5dacfb6ee2df2b946b9bcfc308950
Mark Eibes <mark.eibes@googlemail.com>
2020-07-26 20:07:16 +0200

    Add all runEffectFnN versions

--- purescript-effect/Effect_Uncurried.go
+++ purescript-effect/Effect_Uncurried.go
@@ -21,6 +21,17 @@ func init() {
 		}
 	}
 
+	exports["runEffectFn2"] = func(fn_ Any) Any {
+		return func(a Any) Any {
+			return func(b Any) Any {
+				return func() Any {
+					fn := fn_.(Fn2)
+					return fn(a, b)
+				}
+			}
+		}
+	}
+
 	exports["runEffectFn3"] = func(fn_ Any) Any {
 		return func(a Any) Any {
 			return func(b Any) Any {
@@ -28,6 +39,153 @@ func init() {
 					return func() Any {
 						fn := fn_.(Fn3)
 						return fn(a, b, c)
+					}
+				}
+			}
+		}
+	}
+
+	exports["runEffectFn4"] = func(fn_ Any) Any {
+		return func(a Any) Any {
+			return func(b Any) Any {
+				return func(c Any) Any {
+					return func(d Any) Any {
+						return func() Any {
+							fn := fn_.(Fn4)
+							return fn(a, b, c, d)
+						}
+					}
+				}
+			}
+		}
+	}
+
+	exports["runEffectFn5"] = func(fn_ Any) Any {
+		return func(a Any) Any {
+			return func(b Any) Any {
+				return func(c Any) Any {
+					return func(d Any) Any {
+						return func(e Any) Any {
+							return func() Any {
+								fn := fn_.(Fn5)
+								return fn(a, b, c, d, e)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	exports["runEffectFn6"] = func(fn_ Any) Any {
+		return func(a Any) Any {
+			return func(b Any) Any {
+				return func(c Any) Any {
+					return func(d Any) Any {
+						return func(e Any) Any {
+							return func(f Any) Any {
+								return func() Any {
+									fn := fn_.(Fn6)
+									return fn(a, b, c, d, e, f)
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	exports["runEffectFn7"] = func(fn_ Any) Any {
+		return func(a Any) Any {
+			return func(b Any) Any {
+				return func(c Any) Any {
+					return func(d Any) Any {
+						return func(e Any) Any {
+							return func(f Any) Any {
+								return func(g Any) Any {
+									return func() Any {
+										fn := fn_.(Fn7)
+										return fn(a, b, c, d, e, f, g)
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	exports["runEffectFn8"] = func(fn_ Any) Any {
+		return func(a Any) Any {
+			return func(b Any) Any {
+				return func(c Any) Any {
+					return func(d Any) Any {
+						return func(e Any) Any {
+							return func(f Any) Any {
+								return func(g Any) Any {
+									return func(h Any) Any {
+										return func() Any {
+											fn := fn_.(Fn8)
+											return fn(a, b, c, d, e, f, g, h)
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	exports["runEffectFn9"] = func(fn_ Any) Any {
+		return func(a Any) Any {
+			return func(b Any) Any {
+				return func(c Any) Any {
+					return func(d Any) Any {
+						return func(e Any) Any {
+							return func(f Any) Any {
+								return func(g Any) Any {
+									return func(h Any) Any {
+										return func(i Any) Any {
+											return func() Any {
+												fn := fn_.(Fn9)
+												return fn(a, b, c, d, e, f, g, h, i)
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	exports["runEffectFn10"] = func(fn_ Any) Any {
+		return func(a Any) Any {
+			return func(b Any) Any {
+				return func(c Any) Any {
+					return func(d Any) Any {
+						return func(e Any) Any {
+							return func(f Any) Any {
+								return func(g Any) Any {
+									return func(h Any) Any {
+										return func(i Any) Any {
+											return func(j Any) Any {
+												return func() Any {
+													fn := fn_.(Fn10)
+													return fn(a, b, c, d, e, f, g, h, i, j)
+												}
+											}
+										}
+									}
+								}
+							}
+						}
 					}
 				}
 			}
