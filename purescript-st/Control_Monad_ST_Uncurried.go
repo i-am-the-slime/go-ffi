3198f668c3a0edb268d4c29f82ddb6ca917bca5c
Mark Eibes <mark.eibes@wolt.com>
2024-07-05 00:07:18 +0200

    runSTFn2

--- purescript-st/Control_Monad_ST_Uncurried.go
+++ purescript-st/Control_Monad_ST_Uncurried.go
@@ -14,4 +14,15 @@ func init() {
 			}
 		}
 	}
+
+	exports["runSTFn2"] = func(fn_ Any) Any {
+		return func(a Any) Any {
+			return func(b Any) Any {
+				return func() Any {
+					fn := fn_.(Fn2)
+					return fn(a, b)
+				}
+			}
+		}
+	}
 }
