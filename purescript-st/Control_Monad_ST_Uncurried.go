89d4c32a802b9c3dc8555435f178022bbeae8820
Mark Eibes <mark.eibes@wolt.com>
2024-07-04 23:57:19 +0200

    Add runSTFn1

---
+++ purescript-st/Control_Monad_ST_Uncurried.go
@@ -0,0 +1,17 @@
+package purescript_st
+
+import (
+	. "github.com/purescript-native/go-runtime"
+)
+
+func init() {
+	exports := Foreign("Control.Monad.ST.Uncurried")
+
+	exports.["runSTFn1"] = func(fn Any) Any {
+		return func(a Any) Any {
+			return func() Any {
+				Run(Apply(fn, a))
+			}
+		}
+	}
+}
