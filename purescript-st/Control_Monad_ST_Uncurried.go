c21c28df57ccdbb7b8648d78d5a34814cf47d5bc
Mark Eibes <mark.eibes@wolt.com>
2024-08-18 00:02:37 +0200

    RunSTFns

--- purescript-st/Control_Monad_ST_Uncurried.go
+++ purescript-st/Control_Monad_ST_Uncurried.go
@@ -7,6 +7,13 @@ import (
 func init() {
 	exports := Foreign("Control.Monad.ST.Uncurried")
 
+	// export const runSTFn1 = function runSTFn1(fn) {
+	//   return function(a) {
+	//     return function() {
+	//       return fn(a);
+	//     };
+	//   };
+	// };
 	exports["runSTFn1"] = func(fn Any) Any {
 		return func(a Any) Any {
 			return func() Any {
@@ -25,4 +32,50 @@ func init() {
 			}
 		}
 	}
+
+	exports["runSTFn3"] = func(fn_ Any) Any {
+		return func(a Any) Any {
+			return func(b Any) Any {
+				return func(c Any) Any {
+					return func() Any {
+						fn := fn_.(Fn3)
+						return fn(a, b, c)
+					}
+				}
+			}
+		}
+	}
+
+	exports["runSTFn4"] = func(fn_ Any) Any {
+		return func(a Any) Any {
+			return func(b Any) Any {
+				return func(c Any) Any {
+					return func(d Any) Any {
+						return func() Any {
+							fn := fn_.(Fn4)
+							return fn(a, b, c, d)
+						}
+					}
+				}
+			}
+		}
+	}
+
+	exports["runSTFn5"] = func(fn_ Any) Any {
+		return func(a Any) Any {
+			return func(b Any) Any {
+				return func(c Any) Any {
+					return func(d Any) Any {
+						return func(e Any) Any {
+							return func() Any {
+								fn := fn_.(Fn5)
+								return fn(a, b, c, d, e)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
 }
